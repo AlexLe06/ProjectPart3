308aa8a5cf4aa9d791a29a3316e51cc64fb48e14
AlexLe06 <tanalexle06@gmail.com>
2024-12-03 12:49:00 -0500

    Added comments in AverageDriver.java

--- src/main/java/org/example/AverageDriver.java
+++ src/main/java/org/example/AverageDriver.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class AverageDriver {
     public static void main(String[] args) {
-        Average avg = new Average();
-        avg.calculateMean();
-        avg.selectionSort();
-        System.out.println(avg);
+        Average avg = new Average(); //Creates new average object
+        avg.calculateMean(); //Calls method with constructor
+        avg.selectionSort(); //Calls method with constructor
+        System.out.println(avg); //Prints object
     }
 }
 
