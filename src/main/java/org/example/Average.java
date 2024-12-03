a4846ddf3a6ddbddadbf4d9ec498147ee942d6c0
AlexLe06 <tanalexle06@gmail.com>
2024-12-03 11:41:51 -0500

    Added comments that clarifies the program

--- src/main/java/org/example/Average.java
+++ src/main/java/org/example/Average.java
@@ -2,11 +2,13 @@ package org.example;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
+//Created class Average
 public class Average {
-    private int[] data = new int[5];
+    //Date fields
+    private int[] data = new int[5]; //Array with 5 elements
     private double mean;
 
+    //Created constructor Average
     public Average(){
         Scanner userInput = new Scanner(System.in);
         for (int i = 0; i < data.length; i++){
@@ -16,10 +18,12 @@ public class Average {
             data[i] = score;
         }
     }
+
+    //Methods that calculates average
     public void calculateMean(){
         double sum = 0;
         for (int i = 0; i < data.length; i++){
-            sum += data[i];
+            sum += data[i]; //Adds every element after the loop
         }
         mean = (sum/data.length);
     }
@@ -28,9 +32,11 @@ public class Average {
                 data[0],data[1],data[2],data[3],data[4], mean);
     }
 
+    //Method that sorts the Array
     public void selectionSort(){
-        Arrays.sort(data);
+        Arrays.sort(data); //Sorts array in ascending order
 
+        //Loop that sorts array to descending order
         for (int i = 0; i < data.length / 2; i++) {
             int t = data[i];
             data[i] = data[data.length - i - 1];
